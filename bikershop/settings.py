6d14a91dd2d9908239dd4317574bbe1482272fb3
peterszabo79 <peterszabo79@gmail.com>
2022-10-31 19:32:51 +0000

    addd registration form, set up start to set up reistration

--- bikershop/settings.py
+++ bikershop/settings.py
@@ -134,7 +134,14 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
