141752952a7e505ed3080c13464637ef51cdc3d1
peterszabo79 <peterszabo79@gmail.com>
2022-11-17 12:31:31 +0000

    deployment

--- bikershop/settings.py
+++ bikershop/settings.py
@@ -102,22 +102,22 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 #https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#if 'DATABASE_URL' in os.environ:
-#   DATABASES = {
-#        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#  }
-#else:
-#    DATABASES = {
-#       'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#       }
-#   }
+if 'DATABASE_URL' in os.environ:
+   DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+  }
+else:
+    DATABASES = {
+       'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       }
+   }
 
-DATABASES = {
-    'default': dj_database_url.parse('postgres://wxiwjgqj:g1tGRfp1g79CYcoT4YKLO-zCwpARlLYw@lucky.db.elephantsql.com/wxiwjgqj')
-
-}
+#DATABASES = {
+#    'default': dj_database_url.parse('postgres://wxiwjgqj:g1tGRfp1g79CYcoT4YKLO-zCwpARlLYw@lucky.db.elephantsql.com/wxiwjgqj')
+#
+#}
     
 
 # Password validation
