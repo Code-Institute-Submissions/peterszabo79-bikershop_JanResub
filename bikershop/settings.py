4d98bb42c9794a6986319180a80b757d53734ff4
peterszabo79 <peterszabo79@gmail.com>
2022-11-20 19:19:16 +0000

    static dir change

--- bikershop/settings.py
+++ bikershop/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR  #os.path.join(BASE_DIR, 'static') #BASE_DIR /'static'
 STATICFILES_DIRS = [
-    'static'
+    'bikershop/static'
 ]
 
 MEDIA_URL = '/peterszabo79/' #sthing
