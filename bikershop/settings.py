b72729723b5862a1772ea3b27aaed958aee26989
peterszabo79 <peterszabo79@gmail.com>
2022-11-20 17:14:06 +0000

    remove whitenoise

--- bikershop/settings.py
+++ bikershop/settings.py
@@ -71,7 +71,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -205,9 +204,10 @@ CLOUDINARY_STORAGE = {
 
 }
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
 
 
 
