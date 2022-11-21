b9eab8e227a8a42617043997f02d2e9e3a460fb2
peterszabo79 <peterszabo79@gmail.com>
2022-11-21 20:19:09 +0000

    add new static files dirs

--- bikershop/settings.py
+++ bikershop/settings.py
@@ -182,9 +182,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR #/'static'  #os.path.join(BASE_DIR, 'static') 
-STATICFILES_DIRS = [
-    'static'
-]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/peterszabo79/' 
 
