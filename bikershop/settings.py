dea181f33d8c7311746fc656d34ff37e077c7c01
peterszabo79 <peterszabo79@gmail.com>
2022-12-30 12:05:45 +0000

    added static files to bikershop folder test

--- bikershop/settings.py
+++ bikershop/settings.py
@@ -207,7 +207,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'peterszabo79')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #BASE_DIR #/'static'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'bikershop/static'),
+)
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 
