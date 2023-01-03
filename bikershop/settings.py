d13be3674dd6eaa35399082abef7c66c20c770ff
peterszabo79 <peterszabo79@gmail.com>
2023-01-03 09:12:03 +0000

    test registration link

--- bikershop/settings.py
+++ bikershop/settings.py
@@ -252,7 +252,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'test1peterszabo79@gmail.com'
-print(EMAIL_HOST_USER)
-EMAIL_HOST_PASSWORD = "agff ldyi dzos xmrr"
-DEFAULT_FROM_EMAIL = 'test1peterszabo79@gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
